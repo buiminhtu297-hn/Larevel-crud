7a1c44babfcf2f1a6e0598d180621cfbc451faa8
BMT <buiminhtuladuong@gmail.com>
2021-09-28 15:22:31 +0700

    first-commit

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('todos.list')->with('todos', $todos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('todos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate(request(), [
+        'name' => 'required|min:6|max:12',
+        'description' => 'required'
+        ]);
+
+        $data = request()->all();
+
+        $todo = new Todo();
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->completed = false;
+
+        $todo->save();
+
+        return redirect('/list');    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return view('todos.show')->with('todo', $todo);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        return view('todos.edit')->with('todo', $todo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+         $this->validate(request(), [
+        'name' => 'required|min:6|max:12',
+        'description' => 'required'
+        ]);
+
+        $data = request()->all();
+
+        
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+
+        $todo->save();
+        return redirect('/list');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return redirect('/list');
+    }
+}
---
+++ database/seeders/TodosSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Todo;
+class TodosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Todo::factory()->count(3)->create();
+    }
+}
